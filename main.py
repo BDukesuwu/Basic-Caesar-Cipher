6ae89cd1cedf1686fa8c717867934849f1772b72
Brianna <65559249+BDukesuwu@users.noreply.github.com>
2026-03-05 16:26:48 +0000

    shift the alphabet, starting at the variable assigned to 'shift'. then concatinate with another slice starting before the variable assigned to shift; rearranging the order of the alphabetgit add .

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz' # I will re-arrange the alphabet
 shift = 5 # This will be the new starting index, (f)
-
+shifted_alphabet = alphabet[shift:] + alphabet[:shift]
