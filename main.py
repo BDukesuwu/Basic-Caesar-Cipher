2061f191e7c5756d4895bb4f4cb7aa955dae9fa9
Brianna <65559249+BDukesuwu@users.noreply.github.com>
2026-03-05 16:36:21 +0000

    print program

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ shift = 5 # This will be the new starting index, (f)
 
 # Slice the alphabet starting at the index stored in 'shift' through the end,
 # then concatenate the slice from the start of the alphabet up to 'shift' to rotate it.
-shifted_alphabet = alphabet[shift:] + alphabet[:shift] 
+shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+print(shifted_alphabet)
